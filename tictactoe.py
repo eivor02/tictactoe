0c9a8e0024ca3a71b99f9c155a6e1883ef177149
Saif <136341062+eivor02@users.noreply.github.com>
2023-07-11 13:39:17 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,7 @@ def print_board(board):
         print("|", board[i][0], "|", board[i][1], "|", board[i][2], "|")
         print("-------------")
 
+
 def check_winner(board, player):
     """
     Checks if the specified player has won the game.
